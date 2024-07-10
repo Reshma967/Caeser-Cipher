0f3fdbfda1aa40f4e0a910ec620ea100011e3161
Reshma967 <rs8798@srmist.edu.in>
2024-07-11 02:30:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+logo = """          
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88  
+            88             88                                
+           ""             88                                
+                          88                                
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                            
+              88          
+"""
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+t=True
+while t:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if(shift>26):
+        shift=shift%26
+    def caeser():
+        word=""
+        for i in range(0,len(text)):
+            char=text[i]
+            if char in alphabet:
+                index_letter=alphabet.index(char)
+                if (direction=="encode"):
+                    new_index=index_letter+shift
+                    new=alphabet[new_index]
+                    word=word+new
+                else:
+                    new_index=index_letter-shift
+                    new=alphabet[new_index]
+                    word=word+new
+            else:
+                word=word+char
+        print(f"The {direction}d result is",word)
+        restart=input("Do you want to restart?")
+        if(restart=="no"):
+            t=False
+            print("bye")
+        elif(restart=="yes"):
+            t=True
+    caeser()
